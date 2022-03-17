8947560ab73d105a499aee8b5c718e73a5297d55
ChristopherSimard <chrissimard41@gmail.com>
2022-03-17 12:26:11 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+
+temp = 1
+from gluonts.dataset import common
+from gluonts.model.deepar import DeepAREstimator
+from gluonts.mx.trainer import Trainer
+
+trainer = Trainer(epochs=10)
+estimator = DeepAREstimator(freq='5min', prediction_length=12, trainer=trainer)
+
+
+
+temp = 1
+
